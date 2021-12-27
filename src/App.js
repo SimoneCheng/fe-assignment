41c1471c615b84d3e4e3163a04f164530089ff0c
SimoneCheng <bleach1827@gmail.com>
2021-12-27 14:33:59 +0800

    init [Components] Folder and [Homapage] folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import Homepage from './Components/Homepage';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Homepage />
   );
 }
 
---
+++ src/Components/Homepage/index.js
@@ -0,0 +1,9 @@
+function Homepage() {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
+}
+
+export default Homepage;
